cdad5a9f7c17fc4089672fa5efbc8baed36e8aa5
liuyingjie1130 <1410505543@qq.com>
2019-03-19 16:18:24 +0800

    05

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,85 +2,117 @@ module.exports = function (grunt) {
   grunt.initConfig({
     eslint: {
       options: {
-                configFile: '.eslintrc.json'
-          
+        configFile: '.eslintrc.json'
+
       },
-      target: ['./js/*.js']
+  target: ['*.js']
 
     },
-   csslint: {
+  csslint: {
     options: {
-                      csslintrc: '.csslintrc'
-              
-          
+      csslintrc: '.csslintrc'
+
     },
-          src: ['reactangle.css']
-            
-        
+  src: '*.css'
+
   },
   htmlhint: {
-    html: {
-         options: {
-                        htmlhintrc: '.htmlhintrc'
-                                
-         },
-         src: ['*.html']
-                        
-    
+    options: {
+      htmlhintrc: '.htmlhintrc'
+
     },
-    mocha: {
+    src: '*.html'
+
+  },
+  mocha: {
     test: {
-            src: ['test/index.html'],
-                  
+      src: ['test/index.html'],
+
     },
     options: {
-            run: true,
-                    reporter: 'Spec'
-                          
-    }
-    
+      run: true,
+      reporter: 'Spec'
 
-              
+    }
+
   },
   htmlmin: {
-  options: {
-          collapseWhitespace: true,
-                  preserveLineBreaks: false
-                        
-  },
-  files: {
-          src: './index.html',
-                  dest: 'dist/index.html'
-                        
-  }
-      
+    options: {
+      collapseWhitespace: true,
+      preserveLineBreaks: false
+
+    },
+    files: {
+      src: 'dist/index.html',
+      dest: 'dist/index.html'
+
+    }
+
   },
   cssmin: {
-        'dist/rectangle.css': 'rectangle.css'
-            
+    'dist/rectangle.css': 'rectangle.css'
+
   },
   uglify: {
-  release:{
-  files: {
-            'dist/rectangle.js': 'rectangle.js',
-                      'dist/calc.js': 'calc.js'
-                              
+    release:{
+      files: {
+        'dist/bundle.min.js': 'dist/bundle.js',
+
+      }
+
+    }
+
+  },
+  useminPrepare: {
+    html: 'index.html',
+    options: {
+      dest: 'dist'
+
+    }
+
+  },
+  usemin: {
+    html: ['dist/index.html']
+
+  },
+  concat: {
+    options: {
+      separator: ';'
+
+    },
+    js: {
+      src: ['rectangle.js', 'calc.js'],
+      dest: 'dist/bundle.js'
+
+    }
+
+  },
+  clean: ['dist/bundle.js', '.tmp'],
+  copy: {
+    html: {
+      src: './index.html',
+      dest: './dist/index.html'
+
+    }
+
   }
-        
-  }
+
   });
 
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-htmlhint');
-    grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-     grunt.registerTask('unitTest', ['mocha']);
-     grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unitTest', ['mocha']);
+  grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
 
 };
-
